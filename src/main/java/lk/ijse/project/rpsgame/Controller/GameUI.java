cb78d44e418930023dca669144336e85e091a636
DiilaNa <liyanaarachchidilan@gmail.com>
2025-05-19 17:27:40 +0530

    Main Ui added

--- src/main/java/lk/ijse/project/rpsgame/Controller/GameUI.java
+++ src/main/java/lk/ijse/project/rpsgame/Controller/GameUI.java
@@ -1,4 +1,63 @@
 package lk.ijse.project.rpsgame.Controller;
 
-public class GameUI {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameUI implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    private Label computerResult;
+
+    @FXML
+    private ImageView image;
+
+    @FXML
+    private ImageView imagePaper;
+
+    @FXML
+    private ImageView imageRock;
+
+    @FXML
+    private ImageView imageScissor;
+
+    @FXML
+    private Label playerName;
+
+    @FXML
+    private Label playerResult;
+
+    @FXML
+    private Label result;
+
+    @FXML
+    private ImageView setComputerImage;
+
+    @FXML
+    private ImageView setPlayerImage;
+
+    @FXML
+    void paperBTNAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rockBTNAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void scissorBTNAction(ActionEvent event) {
+
+    }
+
 }
