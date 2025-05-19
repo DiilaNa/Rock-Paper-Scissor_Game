88fd5d9b103561aaea406a34d903673520fee389
DiilaNa <liyanaarachchidilan@gmail.com>
2025-05-19 17:53:57 +0530

    Images set in the Game UI

--- src/main/java/lk/ijse/project/rpsgame/Controller/GameUI.java
+++ src/main/java/lk/ijse/project/rpsgame/Controller/GameUI.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.net.URL;
@@ -12,6 +13,14 @@ import java.util.ResourceBundle;
 public class GameUI implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        Image background = new Image(getClass().getResourceAsStream("/images/RPSBackground.jpg"));
+        image.setImage(background);
+        Image image1 = new Image(getClass().getResourceAsStream("/images/iconStone.png"));
+        imageRock.setImage(image1);
+        Image image2 = new Image(getClass().getResourceAsStream("/images/iconPaper.png"));
+        imagePaper.setImage(image2);
+        Image image3 = new Image(getClass().getResourceAsStream("/images/iconScissors.png"));
+        imageScissor.setImage(image3);
 
     }
 
