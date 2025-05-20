aea849da5ad39900141f7385bdd1b1d97eb9809a
DiilaNa <liyanaarachchidilan@gmail.com>
2025-05-20 14:21:39 +0530

    Login UI added

---
+++ src/main/java/lk/ijse/project/rpsgame/Controller/LoginController.java
@@ -0,0 +1,17 @@
+package lk.ijse.project.rpsgame.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField nameText;
+
+    @FXML
+    void loginAction(ActionEvent event) {
+
+    }
+
+}
